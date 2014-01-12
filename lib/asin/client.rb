8e8d7cb36a9d160a297f78fc5fbe1c1c2e5d2d39
Kyle Carlson <thekylecarlson@gmail.com>
2014-01-12 16:19:02 -0600

    title search

--- lib/asin/client.rb
+++ lib/asin/client.rb
@@ -53,7 +53,7 @@ require 'base64'
 # you can override some defaults or add additional query-parameters to the REST calls:
 #
 #   configure :host => 'webservices.amazon.de'
-#   lookup(asin, :ResponseGroup => :Medium)
+#   lookup(asin, :ResponseGroup => :Large)
 #
 # == Cart
 #
@@ -145,14 +145,14 @@ module ASIN
     #
     # Additional parameters for the API call like this:
     #
-    #   lookup(asin, :ResponseGroup => :Medium)
+    #   lookup(asin, :ResponseGroup => :Large)
     #
     # Or with multiple parameters:
     #
     #   lookup(asin, :ResponseGroup => [:Small, :AlternateVersions])
     #
     def lookup(*asins)
-      params = asins.last.is_a?(Hash) ? asins.pop : {:ResponseGroup => :Medium}
+      params = asins.last.is_a?(Hash) ? asins.pop : {:ResponseGroup => :Large}
       response = call(params.merge(:Operation => :ItemLookup, :ItemId => asins.join(',')))
       arrayfy(response['ItemLookupResponse']['Items']['Item']).map {|item| handle_item(item)}
     end
@@ -174,8 +174,14 @@ module ASIN
     # Have a look at the different search index values on the Amazon-Documentation[http://docs.amazonwebservices.com/AWSECommerceService/latest/DG/index.html]
     #
     def search_keywords(*keywords)
-      params = keywords.last.is_a?(Hash) ? keywords.pop : {:SearchIndex => :Books, :ResponseGroup => :Medium}
+      params = keywords.last.is_a?(Hash) ? keywords.pop : {:SearchIndex => :Books, :ResponseGroup => :Large }
       response = call(params.merge(:Operation => :ItemSearch, :Keywords => keywords.join(' ')))
+      arrayfy(response['ItemSearchResponse']['Items']['Item']).map {|item| handle_item(item)}
+    end
+
+    def search_titles(*keywords)
+      params = keywords.last.is_a?(Hash) ? keywords.pop : {:SearchIndex => :Books, :ResponseGroup => :Large }
+      response = call(params.merge(:Operation => :ItemSearch, :Title => keywords.join(' ')))
       arrayfy(response['ItemSearchResponse']['Items']['Item']).map {|item| handle_item(item)}
     end
 
@@ -193,7 +199,7 @@ module ASIN
     #
     # Have a look at the different search index values on the Amazon-Documentation[http://docs.amazonwebservices.com/AWSECommerceService/latest/DG/index.html]
     #
-    def search(params={:SearchIndex => :Books, :ResponseGroup => :Medium})
+    def search(params={:SearchIndex => :Books, :ResponseGroup => :Large})
       response = call(params.merge(:Operation => :ItemSearch))
       arrayfy(response['ItemSearchResponse']['Items']['Item']).map {|item| handle_item(item)}
     end
@@ -232,7 +238,7 @@ module ASIN
     # Have a look at the optional config values on the Amazon-Documentation[http://docs.amazonwebservices.com/AWSECommerceService/latest/DG/SimilarityLookup.html]
     #
     def similar(*asins)
-      params = asins.last.is_a?(Hash) ? asins.pop : {:SimilarityType => :Random, :ResponseGroup => :Medium}
+      params = asins.last.is_a?(Hash) ? asins.pop : {:SimilarityType => :Random, :ResponseGroup => :Large}
       response = call(params.merge(:Operation => :SimilarityLookup, :ItemId => asins.join(',')))
       arrayfy(response['SimilarityLookupResponse']['Items']['Item']).map {|item| handle_item(item)}
     end
@@ -313,104 +319,104 @@ module ASIN
 
     private
 
-    def arrayfy(item)
-      return [] unless item
-      item.is_a?(Array) ? item : [item]
-    end
-
-    def handle_item(item)
-      handle_type(item, Configuration.item_type)
-    end
-
-    def handle_type(data, type)
-      if type.is_a?(Class)
-        type.new(data)
-      elsif type == :mash
-        require 'hashie'
-        Hashie::Mash.new(data)
-      else
-        data
+      def arrayfy(item)
+        return [] unless item
+        item.is_a?(Array) ? item : [item]
       end
-    end
 
-    def create_item_params(items)
-      keyword_mappings = {
-        :asin               => 'ASIN',
-        :quantity           => 'Quantity',
-        :cart_item_id       => 'CartItemId',
-        :offer_listing_id   => 'OfferListingId',
-        :action             => 'Action'
-      }
-      params = {}
-      items.each_with_index do |item, i|
-        item.each do |key, value|
-          next unless keyword = keyword_mappings[key]
-          params["Item.#{i}.#{keyword}"] = value.to_s
+      def handle_item(item)
+        handle_type(item, Configuration.item_type)
+      end
+
+      def handle_type(data, type)
+        if type.is_a?(Class)
+          type.new(data)
+        elsif type == :mash
+          require 'hashie'
+          Hashie::Mash.new(data)
+        else
+          data
         end
       end
-      params
-    end
 
-    def cart(operation, params={})
-      response = call(params.merge(:Operation => operation))
-      cart = response["#{operation}Response"]['Cart']
-      handle_type(cart, Configuration.cart_type)
-    end
-
-    def call(params)
-      Configuration.validate!
-
-      log(:debug, "calling with params=#{params}")
-      signed = create_signed_query_string(params)
-
-      url = "http://#{Configuration.host}#{PATH}?#{signed}"
-      log(:info, "performing rest call to url='#{url}'")
-
-      response = HTTPI.get(url)
-      if response.code == 200
-        # force utf-8 chars, works only on 1.9 string
-        resp = response.body
-        resp = resp.force_encoding('UTF-8') if resp.respond_to? :force_encoding
-        log(:debug, "got response='#{resp}'")
-        Crack::XML.parse(resp)
-      else
-        log(:error, "got response='#{response.body}'")
-        raise "request failed with response-code='#{response.code}'"
+      def create_item_params(items)
+        keyword_mappings = {
+          :asin               => 'ASIN',
+          :quantity           => 'Quantity',
+          :cart_item_id       => 'CartItemId',
+          :offer_listing_id   => 'OfferListingId',
+          :action             => 'Action'
+        }
+        params = {}
+        items.each_with_index do |item, i|
+          item.each do |key, value|
+            next unless keyword = keyword_mappings[key]
+            params["Item.#{i}.#{keyword}"] = value.to_s
+          end
+        end
+        params
       end
-    end
 
-    def create_signed_query_string(params)
-      # nice tutorial http://cloudcarpenters.com/blog/amazon_products_api_request_signing/
-      params[:Service] = :AWSECommerceService
-      params[:AWSAccessKeyId] = Configuration.key
-      params[:AssociateTag] = Configuration.associate_tag
+      def cart(operation, params={})
+        response = call(params.merge(:Operation => operation))
+        cart = response["#{operation}Response"]['Cart']
+        handle_type(cart, Configuration.cart_type)
+      end
 
-      params[:Version] = Configuration.version unless Configuration.blank? :version
+      def call(params)
+        Configuration.validate!
 
-      # utc timestamp needed for signing
-      params[:Timestamp] = Time.now.utc.strftime('%Y-%m-%dT%H:%M:%SZ')
+        log(:debug, "calling with params=#{params}")
+        signed = create_signed_query_string(params)
+
+        url = "http://#{Configuration.host}#{PATH}?#{signed}"
+        log(:info, "performing rest call to url='#{url}'")
+
+        response = HTTPI.get(url)
+        if response.code == 200
+          # force utf-8 chars, works only on 1.9 string
+          resp = response.body
+          resp = resp.force_encoding('UTF-8') if resp.respond_to? :force_encoding
+          log(:debug, "got response='#{resp}'")
+          Crack::XML.parse(resp)
+        else
+          log(:error, "got response='#{response.body}'")
+          raise "request failed with response-code='#{response.code}'"
+        end
+      end
+
+      def create_signed_query_string(params)
+        # nice tutorial http://cloudcarpenters.com/blog/amazon_products_api_request_signing/
+        params[:Service] = :AWSECommerceService
+        params[:AWSAccessKeyId] = Configuration.key
+        params[:AssociateTag] = Configuration.associate_tag
+
+        params[:Version] = Configuration.version unless Configuration.blank? :version
+
+        # utc timestamp needed for signing
+        params[:Timestamp] = Time.now.utc.strftime('%Y-%m-%dT%H:%M:%SZ')
 
 
-      query = create_query(params)
+        query = create_query(params)
 
-      # yeah, you really need to sign the get-request not the query
-      request_to_sign = "GET\n#{Configuration.host}\n#{PATH}\n#{query}"
-      hmac = OpenSSL::HMAC.digest(DIGEST, Configuration.secret, request_to_sign)
+        # yeah, you really need to sign the get-request not the query
+        request_to_sign = "GET\n#{Configuration.host}\n#{PATH}\n#{query}"
+        hmac = OpenSSL::HMAC.digest(DIGEST, Configuration.secret, request_to_sign)
 
-      # don't forget to remove the newline from base64
-      signature = CGI.escape(Base64.encode64(hmac).chomp)
-      "#{query}&Signature=#{signature}"
-    end
+        # don't forget to remove the newline from base64
+        signature = CGI.escape(Base64.encode64(hmac).chomp)
+        "#{query}&Signature=#{signature}"
+      end
 
-    def create_query(params)
-      params.map do |key, value|
-        value = value.collect{|v| v.to_s.strip}.join(',') if value.is_a?(Array)
-        "#{key}=#{CGI.escape(value.to_s)}"
-      end.sort.join('&').gsub('+','%20') # signing needs to order the query alphabetically
-    end
+      def create_query(params)
+        params.map do |key, value|
+          value = value.collect{|v| v.to_s.strip}.join(',') if value.is_a?(Array)
+          "#{key}=#{CGI.escape(value.to_s)}"
+        end.sort.join('&').gsub('+','%20') # signing needs to order the query alphabetically
+      end
 
-    def log(severity, message)
-      Configuration.logger.send severity, message if Configuration.logger
-    end
+      def log(severity, message)
+        Configuration.logger.send severity, message if Configuration.logger
+      end
   end
 end
